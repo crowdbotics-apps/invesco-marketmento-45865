47056008e7edfa3a0727b5a0dc1a6bc57fc59148
Crowdbotics <team@crowdbotics.com>
2024-02-05 04:37:10 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "invesco-marketmento-45865",
+  url: "https://invesco-marketmento-45865.botics.co",
+  api: "https://invesco-marketmento-45865.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
